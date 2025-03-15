68845e09b3e607a4326f24f07c39dc230adc38ad
victor <victorlopes9291@gmail.com>
2025-03-15 04:48:58 -0300

    v1

--- ByteSoft-Landing-Page/src/App.jsx
+++ ByteSoft-Landing-Page/src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/file.png'
+import QuemSomos from './assets/quemsomos.svg'
+import Gif from './assets/Animation.gif'
 import './App.css'
+import Card1 from "./assets/Card 1.svg"
+import Card2 from "./assets/Card 2.svg"
+import Card3 from "./assets/Card 3.svg"
+import {Form} from "./components/Form/Form.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <div className="card">
+              <img style={{margin: "15px"}} width={250} src={logo} className="logo" alt="ByteSoft Lsogo"/>
+              <div className={"container"} style={{width: "50%", margin: "0 auto", justifyContent: "center",}}>
+                  <h1 style={{color: "white", fontSize: "50px"}}><b>Soluções personalizadas que se adaptam ao seu
+                      negócio</b></h1>
+                  <p style={{color: "white", fontSize: "20px"}}>Transforme suas ideias em realidade com soluções de
+                      software sob medida.</p>
+              </div>
+              <div></div>
+              <div></div>
+          </div>
+          <div className="card-about" style={{display: "flex", alignItems: "center"}}>
+              <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                  <img width={500} src={QuemSomos} className="img" alt="ByteSoft Lsogo"/>
+                  <div className={"card-about-container"} style={{width: "25%", textAlign: "start"}}>
+                      <h2 style={{color: "#252B42", fontSize: "40px"}}><b>Quem Somos</b></h2>
+                      <p style={{color: "#252B42", fontSize: "20px"}}>
+                          A ByteSoft é uma empresa especializada em
+                          desenvolvimento de software, focada em criar
+                          soluções tecnológicas que atendem às necessidades
+                          específicas dos nossos clientes. Com uma equipe de
+                          profissionais experientes e apaixonados por
+                          tecnologia, entregamos produtos de alta qualidade
+                          que ajudam empresas a alcançar seus objetivos.
+                      </p>
+                  </div>
+              </div>
+          </div>
+          <div className="card-descricao" style={{display: "flex"}}>
+              <div className="card-descricao-container" style={{display: "flex", justifyContent: "space-between", alignItems: "center", margin: "0 auto", width: "50%"}}>
+                  <div style={{width: "45%", textAlign: "start"}}>
+                      <h2 style={{color: "#252B42", fontSize: "30px"}}>Por que Escolher a ByteSoft?</h2>
+                      <img src={Card1} alt=""/>
+                      <img src={Card2} alt=""/>
+                      <img src={Card3} alt=""/>
+                  </div>
+                  <img width={400} src={Gif} className="img" alt="ByteSoft Lsogo"/>
+              </div>
+          </div>
+          <div className="card-contato" style={{display: "flex"}}>
+              <div style={{display: "flex", justifyContent: "center", alignItems: "center", margin: "0 auto", width: "30%", flexDirection: "column"}}>
+                  <div>
+                      <h2 style={{color: "white", fontSize: "30px", margin: 0}}>Vamos Transformar Sua Ideia em
+                          Realidade!</h2>
+                      <p style={{color: "white"}}>Entre em contato conosco e descubra como a ByteSoftware pode ajudar o
+                          seu negócio a crescer com soluções de software personalizadas.</p>
+                  </div>
+                  <Form/>
+              </div>
+          </div>
+      </>
   )
 }
 
---
+++ ByteSoft-Landing-Page/src/components/Form/Form.jsx
@@ -0,0 +1,20 @@
+import "./Form.css";
+
+
+
+
+
+
+export const Form = () => {
+    return (
+        <>
+            <form className="form" action={"https://formspree.io/f/mvgkggqy"} method="post">
+                <div className="title2">Entre em contato</div>
+                <input id={"name"} name={"name"} type="text" placeholder="Seu nome" className="input"/>
+                <input id={"email"} name={"email"} type="text" placeholder="Seu email" className="input"/>
+                <textarea id={"message"} name={"message"} placeholder="Sua mensagem"></textarea>
+                <button type={"submit"}>Enviar</button>
+            </form>
+        </>
+    )
+}
